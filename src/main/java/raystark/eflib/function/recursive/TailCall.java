fddf290fbf3d680ac914146b686772ae28afaf73
RayStarkMC <raystarkmc@gmail.com>
2020-06-16 18:57:18 +0900

    リファクタリング isCompletedメソッド移動 TailCall -> TailCallUtil

--- src/main/java/raystark/eflib/function/recursive/TailCall.java
+++ src/main/java/raystark/eflib/function/recursive/TailCall.java
@@ -20,35 +20,112 @@ import static raystark.eflib.function.recursive.TailCallUtil.VOID_COMPLETED;
  * <p>末尾再帰でない再帰関数を定義した場合は最適化されない事に注意してください。そのような再帰関数はコールスタックを保持しておく必要があり、
  * 再帰呼び出しをループに展開することが出来ません。
  *
+ *
+ * <p>最適化された末尾再帰関数を定義するためには再帰関数のreturn文で以下の2つのメソッドを利用してTailCallを実装します。
+ * <ul>
+ *     <li>{@link TailCall#call}: メソッドの再帰呼び出しを行う実装を返します。引数には次に呼び出すメソッドを実行するSupplierを渡します。</li>
+ *     <li>{@link TailCall#complete}: 末尾再帰関数の評価後の値を保持した実装を返します。再帰を抜けるために必ず利用します。</li>
+ * </ul>
+ *
+ * <p>以下に従うことで最適化された末尾再帰関数を定義できます。
+ * <ol>
+ *     <li>関数の戻り値の型をTailCallでラップします。</li>
+ *     <li>終了条件を満たす場合の戻り値を{@link TailCall#complete}メソッドでラップします。</li>
+ *     <li>再帰条件を満たす場合のメソッド呼び出しを{@link TailCall#call}メソッドでラップします。</li>
+ * </ol>
+ *
+ * <p>また、以下の点に注意してください。
+ * <ul>
+ *     <li>return文には{@link TailCall#complete}、{@link TailCall#call}のみを利用してください。</li>
+ *     <li>return文以外で{@link TailCall#complete}、{@link TailCall#call}を利用しないでください。</li>
+ *     <li>再帰関数内で直接自身を呼び出さないでください。</li>
+ * </ul>
+ *
+ * <p>以下に正しく実装された一般フィボナッチ数列の第n項を求める末尾再帰関数fibを示します。
+ * <pre>{@code
+ *      public static void main(String[] args) {
+ *          System.out.println(fib(BigInteger.ZERO, BigInteger.ONE, BigInteger.valueOf(30000)).evaluate());
+ *      }
+ *
+ *      public static TailCall<BigInteger> fib(BigInteger t0, BigInteger t1, BigInteger n) {
+ *          if(n.compareTo(BigInteger.ZERO) < 0) throw new IllegalArgumentException();
+ *          if(n.compareTo(BigInteger.ZERO) == 0) return complete(t0);
+ *          if(n.compareTo(BigInteger.ONE) == 0) return complete(t1);
+ *
+ *          return call(() -> fib(t1, t0.add(t1), n.subtract(BigInteger.ONE)));
+ *      }
+ * }</pre>
+ *
  * <p>末尾再帰関数の実行は{@link TailCall#evaluate}の実行まで遅延されます。
  *
  * @param <T> 末尾再帰関数の戻り値の型
  */
 @FunctionalInterface
 public interface TailCall<T> {
+
+    /**
+     * このTailCallの次に評価されるTailCallを返します。
+     *
+     * @return 次に呼び出されるTailCall
+     */
     @NotNull
     TailCall<T> next();
 
+    /**
+     * このTailCallを評価し、値を取得します。
+     *
+     * <p>メソッドの再起呼び出しはループに変換されます。
+     * 再帰の終了条件が満たされない場合、このメソッドは無限ループに陥る可能性があります。
+     *
+     * <p>いずれかの関数の評価時にスローされた例外は呼び出し元に中継されます。
+     *
+     * @return 末尾再帰関数の戻り値
+     */
     @Nullable
     default T evaluate() {
         for(TailCall<T> tailCall = this; ; tailCall = tailCall.next())
             if(isCompleted(tailCall)) return tailCall.evaluate();
     }
 
+    /**
+     * 再帰呼び出しが修了したTailCallを表すインターフェースです。
+     *
+     * <p>{@link Completed#next}メソッドは自分自身を返し、{@link Completed#evaluate}メソッドは値を返します。
+     *
+     * @param <T> 末尾再帰関数の戻り値の型
+     */
     @FunctionalInterface
     interface Completed<T> extends TailCall<T> {
 
+        /**
+         * このTailCallの次に評価されるTailCallを返します。
+         *
+         * <p>再帰が完了し、次に呼び出すべきTailCallが存在しないため、このメソッドは自身の参照を返します。
+         *
+         * @return 自身の参照
+         */
         @Override
         @NotNull
         default TailCall<T> next() {
             return this;
         }
 
+        /**
+         * このTailCallの値を取得します。
+         *
+         * @return 末尾再帰関数の戻り値
+         */
         @Override
         @Nullable
         T evaluate();
     }
 
+    /**
+     * 与えられたTailCallが終了している場合trueを返します。
+     *
+     * @param tailCall 修了しているか検証するTailCall
+     * @return TailCallが終了している場合true
+     */
     static boolean isCompleted(@NotNull TailCall<?> tailCall) {
         return tailCall instanceof Completed;
     }
--- src/main/java/raystark/eflib/function/recursive/TailCallUtil.java
+++ src/main/java/raystark/eflib/function/recursive/TailCallUtil.java
@@ -1,5 +1,17 @@
 package raystark.eflib.function.recursive;
 
+import org.jetbrains.annotations.NotNull;
+
 class TailCallUtil {
     static final TailCall.Completed<?> VOID_COMPLETED = () -> null;
+
+    /**
+     * 与えられたTailCallが終了している場合trueを返します。
+     *
+     * @param tailCall 修了しているか検証するTailCall
+     * @return TailCallが終了している場合true
+     */
+    static boolean isCompleted(@NotNull TailCall<?> tailCall) {
+        return tailCall instanceof TailCall.Completed;
+    }
 }
