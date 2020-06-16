9a7ac37b9c675531c38c808db1c2dc8655f46e43
RayStarkMC <raystarkmc@gmail.com>
2020-06-17 01:33:52 +0900

    RP1実装

---
+++ src/main/java/raystark/eflib/function/recursive/RP1.java
@@ -0,0 +1,31 @@
+package raystark.eflib.function.recursive;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import raystark.eflib.function.P1;
+
+public interface RP1<T1> {
+    @NotNull
+    BooleanTailCall test(@Nullable T1 t1, @NotNull RP1<T1> self);
+
+    @NotNull
+    default BooleanTailCall test(@Nullable T1 t1) {
+        return test(t1, this);
+    }
+
+    @NotNull
+    static <T1> P1<T1> of(@NotNull RP1<T1> rp1) {
+        return t1 -> rp1.test(t1).evaluate();
+    }
+
+    @NotNull
+    static <T1> P1<T1> of(@NotNull TailCallP1<T1> rp1) {
+        return t1 -> rp1.test(t1).evaluate();
+    }
+
+    @FunctionalInterface
+    interface TailCallP1<T1> {
+        @NotNull
+        BooleanTailCall test(@Nullable T1 t1);
+    }
+}
