e3dac93a4f6b5f758aac81f347be5d250da8baae
RayStarkMC <raystarkmc@gmail.com>
2020-06-25 23:56:51 +0900

    ドキュメント修正

--- src/main/java/raystark/eflib/function/notnull/recursive/NRS.java
+++ src/main/java/raystark/eflib/function/notnull/recursive/NRS.java
@@ -7,6 +7,7 @@ import raystark.eflib.function.notnull.NS;
  * 再帰的に定義された型Tの値のSupplierです。
  *
  * <p>このインターフェースは{@link NRS#get(NRS)}を関数メソッドに持つ関数型インターフェースです。
+ * <p>この関数はnullを扱いません。
  *
  * <p>このインターフェースはSupplier {@link NS}を再帰的ラムダ式で定義するためのインターフェースです。
  * このインターフェース自体のインスタンスからは{@link NS}で定義されるdefaultメソッドにはアクセスできません。
