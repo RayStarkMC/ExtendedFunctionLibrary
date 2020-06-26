1a5ce0f155b46a2daaed6948f7122cd93235358c
RayStarkMC <raystarkmc@gmail.com>
2020-06-26 17:05:55 +0900

    命名法則について言及

--- src/main/java/raystark/eflib/function/notnull/package-info.java
+++ src/main/java/raystark/eflib/function/notnull/package-info.java
@@ -1,8 +1,8 @@
 /**
  * ラムダ式やメソッド参照のターゲットとなる非null関数型インターフェースを提供します。
  *
- * <p>このパッケージ下のインターフェースは{@link raystark.eflib.function}同じ型を持ちますが、
- * nullを扱わないことがアノテーションにより示されています。
+ * <p>このパッケージ下のインターフェースは{@link raystark.eflib.function}同じ型を持ちます。
+ * nullを扱わないことがアノテーションにより示されており、全てのインターフェース名は頭文字にNotNullを表すNが付いています。
  *
  */
 package raystark.eflib.function.notnull;
--- src/main/java/raystark/eflib/function/notnull/recursive/package-info.java
+++ src/main/java/raystark/eflib/function/notnull/recursive/package-info.java
@@ -1,8 +1,8 @@
 /**
  * 非null関数を再帰的に記述するための関数型インターフェースを提供します。
  *
- * <p>このパッケージ下のインターフェースは{@link raystark.eflib.function.recursive}同じ型を持ちますが、
- * nullを扱わないことがアノテーションにより示されています。
+ * <p>このパッケージ下のインターフェースは{@link raystark.eflib.function.recursive}同じ型を持ちます。
+ * nullを扱わないことがアノテーションにより示されており、全てのインターフェース名は頭文字にNotNullを表すNが付いています。
  *
  */
 package raystark.eflib.function.notnull.recursive;
