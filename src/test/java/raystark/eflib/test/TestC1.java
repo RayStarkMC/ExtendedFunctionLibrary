b15664bd370477d13818683d5d4b6a7278b47f92
RayStarkMC <raystarkmc@gmail.com>
2020-10-20 22:18:44 +0900

    関数適用順がわかりやすいように修正

--- src/test/java/raystark/eflib/test/TestC1.java
+++ src/test/java/raystark/eflib/test/TestC1.java
@@ -1,6 +1,5 @@
 package raystark.eflib.test;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import raystark.eflib.function.C1;
@@ -13,72 +12,79 @@ class TestC1 {
     private final MutableData<Integer> data;
 
     TestC1() {
-        this.data = new MutableData<>(0);
+        this.data = new MutableData<>();
     }
 
+    private void addBy(int a) {
+        //noinspection ConstantConditions
+        data.setValue(data.getValue()+a);
+    }
     private void timesBy(int value) {
         //noinspection ConstantConditions
         this.data.setValue(data.getValue()*value);
     }
-
     private void timesBy5() {
         timesBy(5);
     }
-
-    private void set10() {
-        data.setValue(10);
-    }
-
-    @BeforeEach
-    void setUp() {
+    private void setBy0() {
         data.setValue(0);
+    }
+    private void setBy1() {
+        data.setValue(1);
     }
 
     @Test
     void accept(){
-        var c1 = C1.of(data::setValue);
+        setBy0();
+        var c1 = C1.of(this::addBy);
         c1.accept(10);
         assertEquals(10, data.getValue());
     }
 
     @Test
     void nextC1() {
-        var c1 = C1.of(data::setValue).next(this::timesBy);
+        setBy1();
+        var c1 = C1.of(this::addBy).next(this::timesBy);
         c1.accept(10);
-        assertEquals(100, data.getValue());
+        assertEquals(110, data.getValue());
     }
 
     @Test
     void nextA() {
-        var c1 = C1.of(data::setValue).next(this::timesBy5);
+        setBy1();
+        var c1 = C1.of(this::addBy).next(this::timesBy5);
         c1.accept(10);
-        assertEquals(50, data.getValue());
+        assertEquals(55, data.getValue());
     }
 
     @Test
     void prevC1() {
-        var c1 = C1.of(this::timesBy).prev(data::setValue);
+        setBy1();
+        var c1 = C1.of(this::addBy).prev(this::timesBy);
         c1.accept(10);
-        assertEquals(100, data.getValue());
+        assertEquals(20, data.getValue());
     }
 
     @Test
     void prevA() {
-        var c1 = C1.of(this::timesBy).prev(this::set10);
-        c1.accept(5);
-        assertEquals(50, data.getValue());
+        setBy1();
+        var c1 = C1.of(this::addBy).prev(this::timesBy5);
+        c1.accept(10);
+        assertEquals(15, data.getValue());
     }
 
     @Test
     void compose1() {
-        var c1 = C1.of(data::setValue).<String>compose1(Integer::parseInt);
+        setBy0();
+        var c1 = C1.of(this::addBy).<String>compose1(Integer::parseInt);
         c1.accept("10");
         assertEquals(10, data.getValue());
     }
 
     @Test
     void asA() {
-        var a = C1.of(data::setValue).asA(10);
+        setBy0();
+        var a = C1.of(this::addBy).asA(10);
         a.run();
         assertEquals(10, data.getValue());
     }
