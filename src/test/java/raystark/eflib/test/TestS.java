816ba02c4f6cf2599ff176bfbedbf6fd3a06cbed
RayStarkMC <raystarkmc@gmail.com>
2020-10-20 18:43:50 +0900

    TestS記述

---
+++ src/test/java/raystark/eflib/test/TestS.java
@@ -0,0 +1,34 @@
+package raystark.eflib.test;
+
+import org.junit.jupiter.api.Test;
+import raystark.eflib.function.S;
+import raystark.eflib.test.util.MutableData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestS {
+
+    private int supply10() {
+        return 10;
+    }
+
+    @Test
+    void get() {
+        var s = S.of(this::supply10);
+        assertEquals(10, s.get());
+    }
+
+    @Test
+    void then() {
+        var s = S.of(this::supply10).then(String::valueOf);
+        assertEquals("10", s.get());
+    }
+
+    @Test
+    void asA() {
+        var data = new MutableData<>(0);
+        var a = S.of(this::supply10).asA(data::setValue);
+        a.run();
+        assertEquals(10, data.getValue());
+    }
+}
