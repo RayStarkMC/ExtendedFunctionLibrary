1e8ede26ad97691969cc32b8eda93749aeae7e5f
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 22:41:31 +0900

    上限型修正 Exception -> Throwable

--- src/main/java/raystark/eflib/exhandler/function/STh3.java
+++ src/main/java/raystark/eflib/exhandler/function/STh3.java
@@ -3,7 +3,7 @@ package raystark.eflib.exhandler.function;
 import org.jetbrains.annotations.Nullable;
 
 @FunctionalInterface
-public interface STh3<T, X1 extends Exception, X2 extends Exception, X3 extends Exception> {
+public interface STh3<T, X1 extends Throwable, X2 extends Throwable, X3 extends Throwable> {
     @Nullable
     T get() throws X1, X2, X3;
 }
--- src/main/java/raystark/eflib/exhandler/function/STh4.java
+++ src/main/java/raystark/eflib/exhandler/function/STh4.java
@@ -1,6 +1,6 @@
 package raystark.eflib.exhandler.function;
 
 @FunctionalInterface
-public interface STh4<T, X1 extends Exception, X2 extends Exception, X3 extends Exception, X4 extends Exception> {
+public interface STh4<T, X1 extends Throwable, X2 extends Throwable, X3 extends Throwable, X4 extends Throwable> {
     T get() throws X1, X2, X3, X4;
 }
