b4502120b66593c01511bfe8daaa67f2c7e90f3b
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 22:48:56 +0900

    Ath4定義

---
+++ src/main/java/raystark/eflib/exhandler/function/Ath4.java
@@ -0,0 +1,6 @@
+package raystark.eflib.exhandler.function;
+
+@FunctionalInterface
+public interface Ath4<X1 extends Throwable, X2 extends Throwable, X3 extends Throwable, X4 extends Throwable> {
+    void run() throws X1, X2, X3, X4;
+}
