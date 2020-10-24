632c2373f0ceefa36532af19fbf8cd7982f38b5d
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 23:02:16 +0900

    C2定義

---
+++ src/main/java/raystark/eflib/exhandler/function/C2Th3.java
@@ -0,0 +1,8 @@
+package raystark.eflib.exhandler.function;
+
+import org.jetbrains.annotations.Nullable;
+
+@FunctionalInterface
+public interface C2Th3<T1, T2, X1 extends Throwable, X2 extends Throwable, X3 extends Throwable> {
+    void accept(@Nullable T1 t1, @Nullable T2 t2) throws X1, X2, X3;
+}
