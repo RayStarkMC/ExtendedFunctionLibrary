988dcb30f1bacc20d73acfc15051b39da147d5e8
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 23:07:52 +0900

    C4定義

---
+++ src/main/java/raystark/eflib/exhandler/function/C4Th4.java
@@ -0,0 +1,8 @@
+package raystark.eflib.exhandler.function;
+
+import org.jetbrains.annotations.Nullable;
+
+@FunctionalInterface
+public interface C4Th4<T1, T2, T3, T4, X1 extends Throwable, X2 extends Throwable, X3 extends Throwable, X4 extends Throwable> {
+    void accept(@Nullable T1 t1, @Nullable T2 t2, @Nullable T3 t3, @Nullable T4 t4) throws X1, X2, X3, X4;
+}
