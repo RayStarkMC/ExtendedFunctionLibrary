d86276fbc8ec1fb42a4fb58237f23c57f36f3b7a
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 23:35:32 +0900

    ofメソッド消去

--- src/main/java/raystark/eflib/exhandler/Try1.java
+++ src/main/java/raystark/eflib/exhandler/Try1.java
@@ -18,14 +18,6 @@ public interface Try1<T, X1 extends Throwable> {
     );
 
     @NotNull
-    private static <T, X1 extends Throwable> Try1<T, X1>of(
-        @NotNull STh1<T, X1> s,
-        @NotNull Class<X1> classX1
-    ) {
-        return new Try1Impl<>(s, classX1);
-    }
-
-    @NotNull
     static <X1 extends Throwable> Builder<X1> builder(@NotNull Class<X1> throwable1) {
         return new Builder<>() {
             @Override
