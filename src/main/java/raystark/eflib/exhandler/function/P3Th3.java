c273fdecdbc6a628eb9e17cc9b2bb6a87fab532c
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 23:26:24 +0900

    P3定義

---
+++ src/main/java/raystark/eflib/exhandler/function/P3Th3.java
@@ -0,0 +1,8 @@
+package raystark.eflib.exhandler.function;
+
+import org.jetbrains.annotations.Nullable;
+
+@FunctionalInterface
+public interface P3Th3<T1, T2, T3, X1 extends Throwable, X2 extends Throwable, X3 extends Throwable> {
+    boolean test(@Nullable T1 t1, @Nullable T2 t2, @Nullable T3 t3) throws X1, X2, X3;
+}
