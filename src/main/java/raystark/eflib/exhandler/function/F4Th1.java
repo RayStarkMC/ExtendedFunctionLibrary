896dfe3b5c7118dc56680b3c29f8fda4abfe9daa
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 23:17:39 +0900

    F4定義

---
+++ src/main/java/raystark/eflib/exhandler/function/F4Th1.java
@@ -0,0 +1,8 @@
+package raystark.eflib.exhandler.function;
+
+import org.jetbrains.annotations.Nullable;
+
+@FunctionalInterface
+public interface F4Th1<T1, T2, T3, T4, R, X1 extends Throwable> {
+    R apply(@Nullable T1 t1, @Nullable T2 t2, @Nullable T3 t3, @Nullable T4 t4) throws X1;
+}
---
+++ src/main/java/raystark/eflib/exhandler/function/F4Th4.java
@@ -0,0 +1,8 @@
+package raystark.eflib.exhandler.function;
+
+import org.jetbrains.annotations.Nullable;
+
+@FunctionalInterface
+public interface F4Th4<T1, T2, T3, T4, R, X1 extends Throwable, X2 extends Throwable, X3 extends Throwable, X4 extends Throwable> {
+    R apply(@Nullable T1 t1, @Nullable T2 t2, @Nullable T3 t3, @Nullable T4 t4) throws X1, X2, X3, X4;
+}
