49d72aad16af15223dff44ed32779463123cb8cc
RayStarkMC <raystarkmc@gmail.com>
2020-10-24 23:19:11 +0900

    F3定義

---
+++ src/main/java/raystark/eflib/exhandler/function/F3Th2.java
@@ -0,0 +1,8 @@
+package raystark.eflib.exhandler.function;
+
+import org.jetbrains.annotations.Nullable;
+
+@FunctionalInterface
+public interface F3Th2<T1, T2, T3, R, X1 extends Throwable, X2 extends Throwable> {
+    R apply(@Nullable T1 t1, @Nullable T2 t2, @Nullable T3 t3) throws X1, X2;
+}
