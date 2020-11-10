49893846e70a0e283e1cf08c2f67e63c5825c305
RayStarkMC <raystarkmc@gmail.com>
2020-11-10 09:43:06 +0900

    Visitor1各種は不要なため削除

--- src/main/java/raystark/eflib/visitor/acceptor/Acceptor1.java
+++
@@ -1,8 +0,0 @@
-package raystark.eflib.visitor.acceptor;
-
-import org.jetbrains.annotations.NotNull;
-import raystark.eflib.visitor.definition.mono.MonoDefinition1;
-
-public interface Acceptor1<T extends Acceptor1<T, T1>, T1 extends T> {
-    @NotNull <R> R accept(@NotNull MonoDefinition1<T, T1, R> definition1);
-}
