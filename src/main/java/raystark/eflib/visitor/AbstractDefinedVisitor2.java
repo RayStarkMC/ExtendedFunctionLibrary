4fdfe82f4fd35beeb1da45f5d680ee98b414869b
RayStarkMC <raystarkmc@gmail.com>
2020-11-16 23:51:03 +0900

    骨格実装のフィールド削除

--- src/main/java/raystark/eflib/visitor/AbstractDefinedVisitor2.java
+++ src/main/java/raystark/eflib/visitor/AbstractDefinedVisitor2.java
@@ -5,18 +5,12 @@ import raystark.eflib.visitor.acceptor.Acceptor2;
 import raystark.eflib.visitor.definition.IMonoDefinition2;
 
 public abstract class AbstractDefinedVisitor2<T extends Acceptor2<T, T1, T2>, T1 extends T, T2 extends T, R> implements IDefinedVisitor2<T, T1, T2, R> {
-    private final IMonoDefinition2<T, T1, T2, R> definition2;
-
-    protected AbstractDefinedVisitor2(IMonoDefinition2<T, T1, T2, R> definition2) {
-        this.definition2 = definition2;
-    }
+    protected AbstractDefinedVisitor2() {}
 
     @Override
     public final @NotNull R apply(@NotNull T arg1) {
         return arg1.accept(definition2());
     }
 
-    protected @NotNull IMonoDefinition2<T, T1, T2, R> definition2() {
-        return definition2;
-    }
+    protected abstract @NotNull IMonoDefinition2<T, T1, T2, R> definition2();
 }
--- src/main/java/raystark/eflib/visitor/DefinedVisitor2.java
+++ src/main/java/raystark/eflib/visitor/DefinedVisitor2.java
@@ -6,12 +6,19 @@ import raystark.eflib.visitor.acceptor.Acceptor2;
 import raystark.eflib.visitor.definition.MonoDefinition2;
 
 public final class DefinedVisitor2<T extends Acceptor2<T, T1, T2>, T1 extends T, T2 extends T, R> extends AbstractDefinedVisitor2<T, T1, T2, R> {
+    private final MonoDefinition2<T, T1, T2, R> definition2;
+
     private DefinedVisitor2(MonoDefinition2<T, T1, T2, R> definition2) {
-        super(definition2);
+        this.definition2 = definition2;
     }
 
     static <T extends Acceptor2<T, T1, T2>, T1 extends T, T2 extends T, R>
     @NotNull DefinedVisitor2<T, T1, T2, R> build(@NotNull NF1<MonoDefinition2.BuilderT1<T, T1, T2, R>, MonoDefinition2<T, T1, T2, R>> buildProcess) {
         return new DefinedVisitor2<>(MonoDefinition2.build(buildProcess));
+    }
+
+    @Override
+    protected @NotNull MonoDefinition2<T, T1, T2, R> definition2() {
+        return definition2;
     }
 }
