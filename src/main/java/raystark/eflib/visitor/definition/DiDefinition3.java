afc8aa0b3773493d4568cd4aa3cb6f17e19ad0f7
RayStarkMC <raystarkmc@gmail.com>
2020-11-18 10:13:21 +0900

    DiDefinition3インターフェース分離

--- src/main/java/raystark/eflib/visitor/definition/DiDefinition3.java
+++ src/main/java/raystark/eflib/visitor/definition/DiDefinition3.java
@@ -8,67 +8,95 @@ import raystark.eflib.visitor.type.Type1;
 import raystark.eflib.visitor.type.Type2;
 import raystark.eflib.visitor.type.Type3;
 
-public interface DiDefinition3<T extends Acceptor3<T, T1, T2, T3>, T1 extends T, T2 extends T, T3 extends T, R> {
-    @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type1<T1> arg2);
-    @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type2<T2> arg2);
-    @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type3<T3> arg2);
-    @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type1<T1> arg2);
-    @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type2<T2> arg2);
-    @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type3<T3> arg2);
-    @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type1<T1> arg2);
-    @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type2<T2> arg2);
-    @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type3<T3> arg2);
+public class DiDefinition3<T extends Acceptor3<T, T1, T2, T3>, T1 extends T, T2 extends T, T3 extends T, R> implements IDiDefinition3<T, T1, T2, T3, R> {
+    private final NF2<T1, T1, R> f11;
+    private final NF2<T1, T2, R> f12;
+    private final NF2<T1, T3, R> f13;
+    private final NF2<T2, T1, R> f21;
+    private final NF2<T2, T2, R> f22;
+    private final NF2<T2, T3, R> f23;
+    private final NF2<T3, T1, R> f31;
+    private final NF2<T3, T2, R> f32;
+    private final NF2<T3, T3, R> f33;
+
+    private DiDefinition3(
+        NF2<T1, T1, R> f11,
+        NF2<T1, T2, R> f12,
+        NF2<T1, T3, R> f13,
+        NF2<T2, T1, R> f21,
+        NF2<T2, T2, R> f22,
+        NF2<T2, T3, R> f23,
+        NF2<T3, T1, R> f31,
+        NF2<T3, T2, R> f32,
+        NF2<T3, T3, R> f33
+    ) {
+        this.f11 = f11;
+        this.f12 = f12;
+        this.f13 = f13;
+        this.f21 = f21;
+        this.f22 = f22;
+        this.f23 = f23;
+        this.f31 = f31;
+        this.f32 = f32;
+        this.f33 = f33;
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type1<T1> arg2) {
+        return f11.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type2<T2> arg2) {
+        return f12.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type3<T3> arg2) {
+        return f13.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type1<T1> arg2) {
+        return f21.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type2<T2> arg2) {
+        return f22.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type3<T3> arg2) {
+        return f23.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type1<T1> arg2) {
+        return f31.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type2<T2> arg2) {
+        return f32.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
+    @Override
+    public @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type3<T3> arg2) {
+        return f33.apply(arg1.unwrap(), arg2.unwrap());
+    }
+
 
     static <T extends Acceptor3<T, T1, T2, T3>, T1 extends T, T2 extends T, T3 extends T, R>
     @NotNull BuilderT11<T, T1, T2, T3, R> builder() {
         return f11 -> f12 -> f13 ->
                f21 -> f22 -> f23 ->
-               f31 -> f32 -> f33 -> new DiDefinition3<>() {
-            @Override
-            public @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type1<T1> arg2) {
-                return f11.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type2<T2> arg2) {
-                return f12.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type1<T1> arg1, @NotNull Type3<T3> arg2) {
-                return f13.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type1<T1> arg2) {
-                return f21.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type2<T2> arg2) {
-                return f22.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type2<T2> arg1, @NotNull Type3<T3> arg2) {
-                return f23.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type1<T1> arg2) {
-                return f31.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type2<T2> arg2) {
-                return f32.apply(arg1.unwrap(), arg2.unwrap());
-            }
-
-            @Override
-            public @NotNull R dispatch(@NotNull Type3<T3> arg1, @NotNull Type3<T3> arg2) {
-                return f33.apply(arg1.unwrap(), arg2.unwrap());
-            }
-        };
+               f31 -> f32 -> f33 ->
+               new DiDefinition3<>(
+               f11, f12, f13,
+               f21, f22, f23,
+               f31, f32, f33
+        );
     }
 
     static <T extends Acceptor3<T, T1, T2, T3>, T1 extends T, T2 extends T, T3 extends T, R>
