a4e784c7fb30fb28ee93a255ee1dc50de95f1967
RayStarkMC <raystarkmc@gmail.com>
2020-12-06 23:50:30 +0900

    Chainを利用した実装に切り替え

--- src/main/java/raystark/eflib/function/notnull/composer/ComposerNF1.java
+++ src/main/java/raystark/eflib/function/notnull/composer/ComposerNF1.java
@@ -2,45 +2,46 @@ package raystark.eflib.function.notnull.composer;
 
 import org.jetbrains.annotations.NotNull;
 import raystark.eflib.function.notnull.NF1;
+import raystark.eflib.function.notnull.NS;
+import raystark.eflib.util.Chain;
 import raystark.eflib.util.OneShotState;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 public final class ComposerNF1<T1, R> {
-    private final Deque<NF1<?, ?>> functions;
+    private final Chain<NF1<?, ?>> functions;
     private final OneShotState state;
 
+    private static final NS<IllegalStateException> EXCEPTION_SUPPLIER = () -> new IllegalStateException("This composer has already been operated.");
+
     private ComposerNF1(NF1<? super T1, ? extends R> init) {
-        var functions = new LinkedList<NF1<?, ?>>();
-        functions.add(init);
+        var functions = new Chain<NF1<?, ?>>();
+        functions.addFirst(init);
         this.functions = functions;
         this.state = OneShotState.newInstance();
     }
 
-    private ComposerNF1(Deque<NF1<?, ?>> functions) {
+    private ComposerNF1(Chain<NF1<?, ?>> functions) {
         this.functions = functions;
         this.state = OneShotState.newInstance();
     }
 
     public <V> @NotNull ComposerNF1<T1, V> then(@NotNull NF1<? super R, ? extends V> after) {
         return state.transitionThrowing(
-            () -> { functions.offerLast(after); return new ComposerNF1<>(functions); },
-            () -> new IllegalStateException("This composer has already been operated.")
+            () -> { functions.addLast(after); return new ComposerNF1<>(functions); },
+            EXCEPTION_SUPPLIER
         );
     }
 
     public <V1> @NotNull ComposerNF1<V1, R> compose1(@NotNull NF1<? super V1, ? extends T1> before) {
         return state.transitionThrowing(
-            () -> { functions.offerFirst(before); return new ComposerNF1<>(functions); },
-            () -> new IllegalStateException("This composer has already been operated.")
+            () -> { functions.addFirst(before); return new ComposerNF1<>(functions); },
+            EXCEPTION_SUPPLIER
         );
     }
 
     public @NotNull NF1<T1, R> compound() {
         return state.transitionThrowing(
             () -> new CompoundNF1<>(functions),
-            () -> new IllegalStateException("This composer has already been operated.")
+            EXCEPTION_SUPPLIER
         );
     }
 
@@ -52,42 +53,13 @@ public final class ComposerNF1<T1, R> {
     @NotNull ComposerNF1<T1, V> concat(ComposerNF1<? super T1, ? extends R> left, ComposerNF1<? super R, ? extends V> right) {
         var lefts = left.state.transitionThrowing(
             () -> left.functions,
-            () -> new IllegalStateException("This composer has already been operated.")
+            EXCEPTION_SUPPLIER
         );
 
         var rights = right.state.transitionThrowing(
             () -> right.functions,
-            () -> new IllegalStateException("This composer has already been operated.")
+            EXCEPTION_SUPPLIER
         );
-        lefts.addAll(rights);
-        return new ComposerNF1<>(lefts);
-    }
-
-    public static class Mutable<T> {
-        private ComposerNF1<T, T> composer;
-
-        private Mutable(ComposerNF1<T, T> composer) {
-            this.composer = composer;
-        }
-
-        public void then(@NotNull NF1<? super T, ? extends T> after) {
-            composer = composer.then(after);
-        }
-
-        public void compose1(@NotNull NF1<? super T, ? extends T> before) {
-            composer = composer.compose1(before);
-        }
-
-        public @NotNull NF1<T, T> compound() {
-            return composer.compound();
-        }
-
-        public static <T> Mutable<T> newInstance(ComposerNF1<T, T> composer) {
-            return new Mutable<>(composer);
-        }
-
-        public static <T> Mutable<T> concat(Mutable<T> t1, Mutable<T> t2) {
-            return newInstance(ComposerNF1.concat(t1.composer, t2.composer));
-        }
+        return new ComposerNF1<>(Chain.concat(lefts, rights));
     }
 }
--- src/main/java/raystark/eflib/function/notnull/composer/CompoundNF1.java
+++ src/main/java/raystark/eflib/function/notnull/composer/CompoundNF1.java
@@ -2,20 +2,20 @@ package raystark.eflib.function.notnull.composer;
 
 import org.jetbrains.annotations.NotNull;
 import raystark.eflib.function.notnull.NF1;
+import raystark.eflib.util.Chain;
 
-import java.util.Deque;
 import java.util.Iterator;
 
 public class CompoundNF1<T1, R> implements NF1<T1, R> {
-    private final Deque<NF1<?, ?>> functions;
+    private final Chain<NF1<?, ?>> functions;
 
-    CompoundNF1(Deque<NF1<?, ?>> functions) {
+    CompoundNF1(Chain<NF1<?, ?>> functions) {
         this.functions = functions;
     }
 
     @Override
     public @NotNull R apply(@NotNull T1 t1) {
-        Iterator<NF1<?, ?>> iterator = functions.iterator();
+        Iterator<NF1<?, ?>> iterator = functions.asIterable().iterator();
         Object value = t1;
         while(iterator.hasNext()) {
             //functionsはT1 -> T2, T2 -> T3, ... , TN -> Rのように型が連続した関数のリストであるため安全
